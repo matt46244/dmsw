caf0da7ebf36d958fc4cdf4e670659fc0e0dcc5d
matt46244 <github@yeahandstuff.net>
2014-09-02 14:37:14 -0400

    Some ease of reading modifications (ternary operators) and removed some unnecessary string manipulation.

--- dmsw.rb
+++ dmsw.rb
@@ -9,7 +9,7 @@ require 'net/ftp'
 
 require_relative "config"
 
-#did we find the config file
+#did we find the config file?
 raise "You need to create a file called config.rb. See config.rb.example." if !defined? $config_found
 puts "Loaded config okay."
 
@@ -35,8 +35,7 @@ target_week = 1
 
 current_week = 0
 win_lose = ""
-msu_score = 0
-other_score = 0
+score = ""
 url_num = ""
 
 #loop through available fields
@@ -48,15 +47,9 @@ doc.css('tr').each do |row|
 	#split the field into the parts we need
         temp = game.content.split(' ')
         win_lose = temp.first
-	temp2 = temp.last.split('-')
-        if win_lose == "W"
-	  msu_score=temp2.first.to_i
-	  other_score=temp2.last.to_i
-        else
-          other_score=temp2.first_to_i
-          msu_score=temp2.last.to_i
-        end
-        #find the game number to build the URL from later
+	score = temp.last
+
+       #find the espn game number to build the URL from later
         temp = game.to_s
         url_num = temp.match('gameId=(.*)&')[1]
       end
@@ -69,47 +62,32 @@ end
 #debug info
 puts current_week
 puts win_lose
-puts msu_score
-puts other_score
+puts score
 puts url_num
 
 #if we didn't get scores for the correct week, abort
-if !(target_week == current_week)
-  raise "No new scores - nothing to do. Aborting."
-end
+raise "No new scores - nothing to do. Aborting." unless target_week == current_week
 
 # ok, we got a valid score - let's continue
 
 # generate webpage from templates
-
-File.delete("./index.html")
-
 file = File.open("template.1", 'rb')
 html = file.read.chomp
 file.close
+
 file = File.open('template.2', 'rb')
-if win_lose == "W"
-  html.concat("<p class=\"yes\">Yes.")
-else
-  html.concat("<p class=\"no\>No.")
-end
+win_lose == "W" ? html.concat("<p class=\"yes\">Yes.") : html.concat("<p class=\"no\>No.")
 html.concat(file.read.chomp)
 file.close
-html.concat(url_num)
-html.concat("\" target=\"_blank\">")
-if win_lose == "W"
-  html.concat(msu_score.to_s)
-  html.concat("-")
-  html.concat(other_score.to_s)
-else
-  html.concat(other_score.to_s)
-  html.concat("-")
-  html.concat(msu_score.to_s)
-end
+
+html.concat(url_num + "\" target=\"_blank\">" + score)
+
 file = File.open('template.3', 'rb')
 html.concat(file.read.chomp)
 file.close
 
+#ok, delete the old file and write the new file
+File.delete("./index.html")
 index = File.open('index.html', 'w')
 index.write(html)
 index.close
@@ -121,17 +99,8 @@ ftp.putbinaryfile('index.html')
 ftp.close
 
 # generate the tweet for posting
-if win_lose == "W"
-tweet = "YES. "
-tweet.concat(msu_score.to_s)
-tweet.concat("-")
-tweet.concat(other_score.to_s)
-else
-tweet = "NO. "
-tweet.concat(other_score.to_s)
-tweet.concat("-")
-tweet.concat(msu_score.to_s)
-end
+win_lose == "W" ? tweet = "YES. " + score : tweet = "NO. " + score
+
 tweet.concat(". didmichiganstatewin.com")
 
 # write tweet to twitter
