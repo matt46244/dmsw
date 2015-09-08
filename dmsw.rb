456c48eec2ac03662bab38f6dcd258eeef042e34
matt46244 <github@yeahandstuff.net>
2015-09-07 23:05:05 -0400

    Removing some of the meta info and updated week.

--- dmsw.rb
+++ dmsw.rb
@@ -9,7 +9,7 @@ $log = Logger.new(STDOUT)
 $log.level = Logger::INFO
 
 #what week are we currently in? we only want to get the scores from the current week
-target_week = 1
+target_week = 2
 
 #get webpage from espn for parsing
 game = Game.new("", "", "", 0, "fb")
